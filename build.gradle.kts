84c23f2d5fc57ad2ead8d5d25ef119aea725b34d
benkuly <12199167+benkuly@users.noreply.github.com>
2020-05-20 18:16:28 +0200

    added version tag to docker image

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ allprojects {
     apply(plugin = "kotlin")
 
     group = "net.folivo"
-    version = "0.1.0.RELEASE"
+    version = "0.1.1.RELEASE"
     java.sourceCompatibility = JavaVersion.VERSION_11
 
     repositories {
