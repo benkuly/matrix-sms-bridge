d3c58d6a0d765339e38cfea55b1a96c14b35edca
benkuly <12199167+benkuly@users.noreply.github.com>
2020-08-31 14:52:49 +0200

    updated Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,14 +1,14 @@
 object Versions {
-    const val kotlin = "1.3.72"
-    const val kotlinxCoroutines = "1.3.7"
+    const val kotlin = "1.4.0"
+    const val kotlinxCoroutines = "1.3.9"
     const val kotlinRetry = "1.0.5"
-    const val springBoot = "2.3.1.RELEASE"
+    const val springBoot = "2.3.3.RELEASE"
     const val springDependencyManagement = "1.0.9.RELEASE"
-    const val springMockk = "2.0.1"
+    const val springMockk = "2.0.3"
     const val matrixSDK = "0.3.1.RELEASE"
-    const val clikt = "2.7.1"
+    const val clikt = "2.8.0"
     const val ant = "1.10.8"
-    const val libphonenumber = "8.12.5"
-    const val neo4jrx = "1.1.0"
-    const val testcontainers = "1.14.1"
+    const val libphonenumber = "8.12.8"
+    const val testcontainers = "1.14.3"
+    const val neo4jrx = "1.1.1" // TODO deprecated
 }
