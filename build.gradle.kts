689d356ae90e474ae2e3a63644da44d38426dbf8
benkuly <12199167+benkuly@users.noreply.github.com>
2021-04-27 14:33:30 +0200

    compatibility fix for synapse 1.32

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 group = "net.folivo"
-version = "0.5.6"
+version = "0.5.7"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootJar>() {
